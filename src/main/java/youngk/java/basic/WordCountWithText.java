72d33622249edc4cf75ffcaa01b212422d53fbd1
yangkai <yangkai@huoyunren.com>
2019-07-11 12:54:45 +0800

    bugfixed

--- src/main/java/youngk/java/basic/WordCountWithText.java
+++ src/main/java/youngk/java/basic/WordCountWithText.java
@@ -8,6 +8,12 @@ import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 /**
  * FROM youngk
  *
@@ -31,7 +37,20 @@ public class WordCountWithText {
     public static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>>{
 
         @Override
-        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws IOException {
+            URL resource = this.getClass().getClassLoader().getResource("aa.txt");
+            System.out.println(resource.getPath());
+            File file = new File("src/main/resources/aa.txt");
+            InputStream stream = new FileInputStream(file);
+            int  n=0;
+            StringBuffer sBuffer=new StringBuffer();
+            while (n!=-1)
+            {
+                n=stream.read();
+                char by=(char)n;
+                sBuffer.append(by);
+            }
+            System.out.println(sBuffer.toString());
             String[] words = value.split(" ");
             for(String word : words) {
                 out.collect(new Tuple2<>(word, 1));
